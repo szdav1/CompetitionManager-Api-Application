d18950d55109e7fc41c83597a5bd84b8299aa5c6
szdav1 <szantodavid.mail@gmail.com>
2025-04-06 13:59:36 +0200

    Add scheme.sql.

---
+++ schema.sql
@@ -0,0 +1,31 @@
+-- -- Create table competition
+-- CREATE TABLE competition
+-- (
+--     id       BIGINT          NOT NULL AUTO_INCREMENT,
+--     name     varchar(255) NOT NULL,
+--     location varchar(255) NOT NULL,
+--     date     date         NOT NULL,
+--     PRIMARY KEY (id)
+-- );
+--
+-- -- Create table competitor
+-- CREATE TABLE competitor
+-- (
+--     id         BIGINT          NOT NULL AUTO_INCREMENT,
+--     name       varchar(255) NOT NULL,
+--     club       varchar(255) NOT NULL,
+--     birth_date date         NOT NULL,
+--     PRIMARY KEY (id)
+-- );
+--
+-- -- Create table leaderboard
+-- CREATE TABLE leaderboard
+-- (
+--     id             BIGINT NOT NULL AUTO_INCREMENT,
+--     competition_id BIGINT NOT NULL,
+--     competitor_id  BIGINT NOT NULL,
+--     placement      int NOT NULL,
+--     PRIMARY KEY (id),
+--     foreign key (competition_id) references competition (id) on delete cascade on update cascade,
+--     foreign key (competitor_id) references competitor (id) on delete cascade on update cascade
+-- );
