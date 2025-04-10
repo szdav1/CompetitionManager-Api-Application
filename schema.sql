425cf6ae3f3a52da9d76ade9c9563f99f32be489
szdav1 <szantodavid.mail@gmail.com>
2025-04-10 21:25:27 +0200

    Update Api.

--- schema.sql
+++ schema.sql
@@ -1,31 +1,39 @@
--- -- Create table competition
--- CREATE TABLE competition
--- (
---     id       BIGINT          NOT NULL AUTO_INCREMENT,
---     name     varchar(255) NOT NULL,
---     location varchar(255) NOT NULL,
---     date     date         NOT NULL,
---     PRIMARY KEY (id)
--- );
---
--- -- Create table competitor
--- CREATE TABLE competitor
--- (
---     id         BIGINT          NOT NULL AUTO_INCREMENT,
---     name       varchar(255) NOT NULL,
---     club       varchar(255) NOT NULL,
---     birth_date date         NOT NULL,
---     PRIMARY KEY (id)
--- );
---
--- -- Create table leaderboard
--- CREATE TABLE leaderboard
--- (
---     id             BIGINT NOT NULL AUTO_INCREMENT,
---     competition_id BIGINT NOT NULL,
---     competitor_id  BIGINT NOT NULL,
---     placement      int NOT NULL,
---     PRIMARY KEY (id),
---     foreign key (competition_id) references competition (id) on delete cascade on update cascade,
---     foreign key (competitor_id) references competitor (id) on delete cascade on update cascade
--- );
+-- Create table competition
+CREATE TABLE competition
+(
+    id       BIGINT          NOT NULL AUTO_INCREMENT,
+    name     varchar(255) NOT NULL,
+    location varchar(255) NOT NULL,
+    date     date         NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Create table competitor
+CREATE TABLE competitor
+(
+    id         BIGINT          NOT NULL AUTO_INCREMENT,
+    name       varchar(255) NOT NULL,
+    club       varchar(255) NOT NULL,
+    birth_date date         NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Create table leaderboard
+CREATE TABLE leaderboard
+(
+    id             BIGINT NOT NULL AUTO_INCREMENT,
+    competition_id BIGINT NOT NULL,
+    competitor_id  BIGINT NOT NULL,
+    placement      int NOT NULL,
+    PRIMARY KEY (id),
+    foreign key (competition_id) references competition (id) on delete cascade on update cascade,
+    foreign key (competitor_id) references competitor (id) on delete cascade on update cascade
+);
+
+CREATE TABLE admins
+(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL
+    PRIMARY KEY (id)
+);
