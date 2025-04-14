cbb4f56b9955d5903c3811a091cfbe0d03814da4
szdav1 <szantodavid.mail@gmail.com>
2025-04-15 00:25:32 +0200

    Implement authentication.

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
--- Create table competition
+-- -- Create table competition
 CREATE TABLE competition
 (
-    id       BIGINT          NOT NULL AUTO_INCREMENT,
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
     name     varchar(255) NOT NULL,
     location varchar(255) NOT NULL,
     date     date         NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE competition
 -- Create table competitor
 CREATE TABLE competitor
 (
-    id         BIGINT          NOT NULL AUTO_INCREMENT,
+    id         BIGINT       NOT NULL AUTO_INCREMENT,
     name       varchar(255) NOT NULL,
     club       varchar(255) NOT NULL,
     birth_date date         NOT NULL,
@@ -24,16 +24,18 @@ CREATE TABLE leaderboard
     id             BIGINT NOT NULL AUTO_INCREMENT,
     competition_id BIGINT NOT NULL,
     competitor_id  BIGINT NOT NULL,
-    placement      int NOT NULL,
+    placement      int    NOT NULL,
     PRIMARY KEY (id),
     foreign key (competition_id) references competition (id) on delete cascade on update cascade,
     foreign key (competitor_id) references competitor (id) on delete cascade on update cascade
 );
 
-CREATE TABLE admins
+-- Create table users
+CREATE TABLE users
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    role     varchar(255) NOT NULL,
     username varchar(255) NOT NULL,
-    password varchar(255) NOT NULL
+    password varchar(255) NOT NULL,
     PRIMARY KEY (id)
 );
